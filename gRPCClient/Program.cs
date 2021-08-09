75cc5e0491341c6aa199c284cfad14f5e4309b1c
ericrkuo <ericrkuo@gmail.com>
2021-08-08 17:20:13 -0700

    Add server side streaming

--- gRPCClient/Program.cs
+++ gRPCClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Grpc.Core;
 using Grpc.Net.Client;
 using gRPCCommon;
 
@@ -9,18 +10,41 @@ namespace GrpcGreeterClient
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Press enter to start");
-            Console.ReadLine();
+            Console.Write("Select type of call: Unary[0], Server[1]: ");
+            String option = Console.ReadLine();
 
             // The port number(5001) must match the port of the gRPC server.
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-
             var client = new gRPCDemo.gRPCDemoClient(channel);
-            var reply = await client.UnaryCallAsync(new SampleRequest { Name = "SampleName" });
-            Console.WriteLine("Client received reply with message: {0}", reply.Message);
+
+            switch (option)
+            {
+                case "0":
+                    await UnaryCallDemo(client);
+                    break;
+                case "1":
+                    await ServerStreamDemo(client);
+                    break;
+            }
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
+        }
+
+        private static async Task ServerStreamDemo(gRPCDemo.gRPCDemoClient client)
+        {
+            using var call = client.ServerSideStream(new SampleRequest { Name = "GreeterClient" });
+
+            await foreach (var reply in call.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine("Server is streaming message to client: {0}", reply.Message);
+            }
+        }
+
+        private static async Task UnaryCallDemo(gRPCDemo.gRPCDemoClient client)
+        {
+            var reply = await client.UnaryCallAsync(new SampleRequest { Name = "SampleName" });
+            Console.WriteLine("Client received reply with message: {0}", reply.Message);
         }
 
         private static async Task GreeterClient(GrpcChannel channel)
--- gRPCServer/Services/GrpcDemoService.cs
+++ gRPCServer/Services/GrpcDemoService.cs
@@ -16,10 +16,26 @@ namespace gRPCServer.Services
         public override Task<SampleReply> UnaryCall(SampleRequest request, ServerCallContext context)
         {
             _logger.LogInformation("Received unary call from client with name {0}", request.Name);
+
             return Task.FromResult(new SampleReply
             {
                 Message = "Hello " + request.Name
             });
+        }
+
+        public override async Task ServerSideStream(SampleRequest request, IServerStreamWriter<SampleReply> responseStream, ServerCallContext context)
+        {
+            _logger.LogInformation("Received server streaming call from client with name {0}", request.Name);
+
+            foreach (char c in request.Name.ToCharArray())
+            {
+                var reply = new SampleReply()
+                {
+                    Message = string.Format("Streaming letter by letter: {0}", c)
+                };
+                await responseStream.WriteAsync(reply);
+                await Task.Delay(500);
+            }
         }
     }
 }
