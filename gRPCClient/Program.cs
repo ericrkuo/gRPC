e67062eb823b4b022a70095dcf8fff87b53df0f6
ericrkuo <ericrkuo@gmail.com>
2021-08-08 17:00:41 -0700

    Add unary call

--- gRPCClient/Program.cs
+++ gRPCClient/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Grpc.Net.Client;
 using gRPCCommon;
@@ -15,12 +14,21 @@ namespace GrpcGreeterClient
 
             // The port number(5001) must match the port of the gRPC server.
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new gRPCDemo.gRPCDemoClient(channel);
+            var reply = await client.UnaryCallAsync(new SampleRequest { Name = "SampleName" });
+            Console.WriteLine("Client received reply with message: {0}", reply.Message);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static async Task GreeterClient(GrpcChannel channel)
+        {
             var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
                               new HelloRequest { Name = "GreeterClient" });
             Console.WriteLine("Greeting: " + reply.Message);
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
         }
     }
 }
---
+++ gRPCServer/Services/GrpcDemoService.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+using gRPCCommon;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace gRPCServer.Services
+{
+    public class GrpcDemoService: gRPCDemo.gRPCDemoBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GrpcDemoService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<SampleReply> UnaryCall(SampleRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation("Received unary call from client with name {0}", request.Name);
+            return Task.FromResult(new SampleReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
